7b3fcbe13c4ff1f193c8dd172b207a7c115c3317
Роман <ram23@mail.ru>
2022-06-25 18:26:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Collection_Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            string text = File.ReadAllText("C:\\Users\\Роман\\Desktop\\cdev_Text.txt");
+
+            char[] chars = text.ToCharArray();
+
+            List<char> list = new List<char>(chars);
+
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+
+            ///////////////////////////////////////////////////////////////////////
+
+            var stopWatch1 = Stopwatch.StartNew();
+
+            string text1 = File.ReadAllText("C:\\Users\\Роман\\Desktop\\cdev_Text.txt");
+
+            char[] chars1 = text.ToCharArray();
+
+            LinkedList<char> list1 = new LinkedList<char>(chars);
+
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+
+        }
+    }
+}
